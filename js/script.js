f3298af4fa2d61d669870d7cb4b173d36444261a
contreras48 <contreras4893@gmail.com>
2025-03-23 15:01:52 -0600

    Initial commit: setup project structure.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const anchors = document.querySelectorAll('a');
+
+/* anchors.forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+
+    });
+}); */
